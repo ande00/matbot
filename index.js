5e3393babcbb9609131a65b1c8e776afcfb3c985
ande00 <165920861+ande00@users.noreply.github.com>
2024-10-23 18:57:12 +0300

    vittu

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ async function createBot() {
     version: "1.12.2",
   });
 
-  bot.on('messagestr', (message, messagePosition, jsonMsg, sender, verified) => {
+  bot.on('messagestr', async (message, messagePosition, jsonMsg, sender, verified) => {
   const channel = await client.channels.fetch("1298675034264436879");
   channel.send(message)
 })
