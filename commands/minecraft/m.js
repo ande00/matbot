0a7342477cb25617b7d286494a6633136dbb7e44
ande00 <165920861+ande00@users.noreply.github.com>
2024-10-24 16:47:54 +0300

    Update m.js

--- commands/minecraft/m.js
+++ commands/minecraft/m.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
 const get = require("../../tasks/mats.task");
 const vec3 = require("vec3");
 
 module.exports = {
   async execute(bot, message, username) {
+    const xyz = process.env.STASH.split(',')
+    const pos = new vec3(xyz);
+    if(bot.entity.position.distanceTo(pos) < 10) bot.chat("/kill");
     const args = message.split(" ");
     const order = args[1];
     const tp = args[2];
